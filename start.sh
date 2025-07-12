4123fe7acd3baa714bb82e78cf686f425f197691
fritz-bautista <evodark93@gmail.com>
2025-07-13 01:53:05 +0800

    'ayokonatalaga'

--- start.sh
+++ start.sh
@@ -1,11 +1,3 @@
-#!/usr/bin/env bash
-
-# Laravel prep
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-php artisan migrate --force
-
-# Start services
-php-fpm &
-nginx -g 'daemon off;'
+#!/bin/bash
+php-fpm8.1 -D        # Start PHP-FPM in daemon mode
+nginx -g 'daemon off;'  # Start NGINX in foreground for container
