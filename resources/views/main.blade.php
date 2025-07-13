bc98136af97355a98321b084ff806e62304da072
fritz-bautista <evodark93@gmail.com>
2025-07-13 19:41:45 +0800

    css_test_6

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- ✅ Mobile responsive -->
@@ -15,10 +16,10 @@
 <body>
     <div class="Nav-Bar">
         <div class="Nav-Bar-TopRow">
+            <button class="nav-toggle" onclick="toggleNav()">☰</button>
             <a class="Logo-link" href="{{ route('home') }}">
                 <img class="Logo" src="{{ asset('img/Logo.png') }}" alt="Logo">
             </a>
-            <button class="nav-toggle" onclick="toggleNav()">☰</button>
         </div>
 
         <div class="Nav-Bar-Category-Container" id="mobileNav">
